3cbe4034de434bc672ade873b989c7ab9a87566f
Weeco <weeco91@gmail.com>
2018-04-28 12:15:52 +0200

    Added more MessageStatus codes

--- src/rpc-queue/enums/message-status.ts
+++ src/rpc-queue/enums/message-status.ts
@@ -1,5 +1,9 @@
 export enum MessageStatus {
   Success = 200,
+  BadRequest = 400,
+  Unauthorized = 401,
+  Forbidden = 403,
   UnprocessableEntity = 422,
-  ServerError = 500
+  ServerError = 500,
+  ServiceUnavailable = 503
 }
